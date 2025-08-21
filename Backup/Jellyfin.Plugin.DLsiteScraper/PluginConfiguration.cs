be8eff66e4be2de41fbec6746c03a4f9edc5cc5a
zuozq <youxiaozzq@126.com>
2025-08-22 00:08:32 +0800

    remove redundant files

--- Backup/Jellyfin.Plugin.DLsiteScraper/PluginConfiguration.cs
+++
@@ -1,7 +0,0 @@
-using MediaBrowser.Model.Plugins;
-
-namespace Jellyfin.Plugin.DLsiteScraper;
-
-public class PluginConfiguration : BasePluginConfiguration
-{
-}
--- Backup/Jellyfin.Plugin.HanimeScraper/PluginConfiguration.cs
+++
@@ -1,7 +0,0 @@
-using MediaBrowser.Model.Plugins;
-
-namespace Jellyfin.Plugin.HanimeScraper;
-
-public class PluginConfiguration : BasePluginConfiguration
-{
-}
